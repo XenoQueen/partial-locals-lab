076f713a9f100ef97377318d97d73fdebe179773
XenoQueen <queenmab051@gmail.com>
2021-07-19 03:48:44 -0700

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,12 @@
-# == Schema Information
-#
-# Table name: students
-#
-#  id         :integer          not null, primary key
-#  name       :string
-#  hometown   :string
-#  birthday   :datetime
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(term)
+    if term == ""
+      return Student.all
+    else
+      return Student.all.filter{|s| s.name.downcase.include?(term.downcase) }
+    end 
+  end
 end
